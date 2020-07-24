8310a5ef12fc14648f71282b87dec4b12c706217
Angel D. Munoz <scythes.lol@gmail.com>
2020-07-24 00:56:02 -0600

    add types

---
+++ Types.fs
@@ -0,0 +1,30 @@
+namespace Resumaker
+
+module Types =
+
+    type Link = { Name: string; Url: string }
+
+    type Project =
+        { Name: string
+          ToolOrStack: seq<string>
+          Url: string }
+
+    type Job = { Employer: string; Summary: string }
+
+    type Skill = { Name: string; Years: int }
+
+    type Profile =
+        { Name: string
+          LastName: string
+          PersonalSummary: string
+          Objectives: string
+          Picture: string
+          Email: string }
+
+    type Resume =
+        { Profile: Profile
+          Skills: seq<Skill>
+          PreviousJobs: seq<Job>
+          Projects: seq<Project>
+          DevLinks: seq<Link>
+          SocialMedia: seq<Link> }
